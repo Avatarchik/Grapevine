5300d101c64a32ce174187ceb24121c52aaa0352
Jenny Yang <hyeayouy@gmail.com>
2015-12-06 15:40:26 -0500

    Integrated Swipe view example

---
+++ Grapevine/OverlayView.swift
@@ -0,0 +1,52 @@
+//
+//  OverlayView.swift
+//  Grapevine
+//
+//  Referenced from TinderSwipeCardsSwift created by Gao Chao
+//  https://github.com/reterVision/TinderSwipeCardsSwift
+//
+//  Created by Jenny Yang on 12/6/15.
+//  Copyright © 2015 Jenny Yang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum GGOverlayViewMode {
+    case GGOverlayViewModeLeft
+    case GGOverlayViewModeRight
+}
+
+class OverlayView: UIView{
+    var _mode: GGOverlayViewMode! = GGOverlayViewMode.GGOverlayViewModeLeft
+    var imageView: UIImageView!
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.whiteColor()
+        imageView = UIImageView(image: UIImage(named: "noButton"))
+        self.addSubview(imageView)
+    }
+    
+    func setMode(mode: GGOverlayViewMode) -> Void {
+        if _mode == mode {
+            return
+        }
+        _mode = mode
+        
+        if _mode == GGOverlayViewMode.GGOverlayViewModeLeft {
+            imageView.image = UIImage(named: "noButton")
+        } else {
+            imageView.image = UIImage(named: "yesButton")
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.frame = CGRectMake(50, 50, 100, 100)
+    }
+}
--- Grapevine/SwipeViewController.swift
+++ Grapevine/SwipeViewController.swift
@@ -13,7 +13,8 @@ class SwipeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        var draggableBackground: DraggableViewBackground = DraggableViewBackground(frame: self.view.frame)
+        self.view.addSubview(draggableBackground)
     }
 
     override func didReceiveMemoryWarning() {
